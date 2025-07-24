098cec802d3293cf362a3a7c467964c8d4673b92
kimitakanabe <kimiko.takanabe@gmail.com>
2025-07-24 13:37:51 +0200

    Add css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <!-- Font Awesome -->
         <script src="https://kit.fontawesome.com/4b610f9626.js" crossorigin="anonymous"></script>
+
+        <link href="{{ asset('css/samuraimart.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
