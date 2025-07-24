e63582fc3296f77db9ad4dc748ef46c78dce7b36
kimitakanabe <kimiko.takanabe@gmail.com>
2025-07-24 15:06:26 +0200

    Footer component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,12 @@
            @component('components.header')
            @endcomponent
 
-            <main class="py-4">
+            <main class="py-4 mb-5">
                 @yield('content')
             </main>
 
+           @component('components.footer')
+           @endcomponent
         </div>
         <!-- Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
