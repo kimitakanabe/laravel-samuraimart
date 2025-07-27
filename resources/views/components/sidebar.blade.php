c3d9a26cd7792121c8c3851040757d49ea6728bb
kimitakanabe <kimiko.takanabe@gmail.com>
2025-07-27 17:20:34 +0200

    Add Filter function

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,7 +4,7 @@
         @foreach ($categories as $category)
             @if ($category->major_category_name === $major_category_name)
                 <label class="samuraimart-sidebar-category-label">
-                    <a href="#">{{ $category->name }}</a>
+                    <a href="{{ route('products.index', ['category' => $category->id]) }}">{{ $category->name }}</a>
                 </label>
             @endif
         @endforeach
