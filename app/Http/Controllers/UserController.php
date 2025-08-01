ba280fa02cfc60f442244eea6bfb981c0bbfe0ac
kimitakanabe <kimiko.takanabe@gmail.com>
2025-08-01 18:18:49 +0200

    Add favorite action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,4 +73,13 @@ class UserController extends Controller
     {
         return view('users.edit_password');
     }
+
+    public function favorite()
+    {
+        $user = Auth::user();
+
+        $favorite_products = $user->favorite_products;
+
+        return view('users.favorite', compact('favorite_products'));
+    }
 }
