120a8aa87867e17ed14c38fff85ba3ba88288fa0
kimitakanabe <kimiko.takanabe@gmail.com>
2025-08-07 13:15:17 +0200

    Create CategoryController

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,6 +1,10 @@
 <?php
 
+// AdminとRouteに "Undefined type" エラーが出たため教材にはないが、use宣言を追記
+use Illuminate\Support\Facades\Route;
+use Encore\Admin\Facades\Admin;
 use Illuminate\Routing\Router;
+use App\Admin\Controllers\CategoryController;
 
 Admin::routes();
 
@@ -12,5 +16,6 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('categories', CategoryController::class);
 
 });
