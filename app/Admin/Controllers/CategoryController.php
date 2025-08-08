bd5de355243ef595e4649f11c72adc112ca8bebf
kimitakanabe <kimiko.takanabe@gmail.com>
2025-08-08 17:06:18 +0200

    Edit CategoryController

--- app/Admin/Controllers/CategoryController.php
+++ app/Admin/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Category;
+use App\Models\MajorCategory;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -31,14 +32,14 @@ class CategoryController extends AdminController
         $grid->column('id', __('Id'))->sortable();
         $grid->column('name', __('Name'));
         $grid->column('description', __('Description'));
-        $grid->column('major_category_name', __('Major category name'));
+        $grid->column('major_category_id', __('Major category name'))->editable('select', MajorCategory::all()->pluck('name', 'id'));
         $grid->column('created_at', __('Created at'))->sortable();
         $grid->column('updated_at', __('Updated at'))->sortable();
 
         $grid->filter(function($filter){
             // 部分一致のフィルタを追加する関数。第1引数にカラム名、第2引数に画面に表示する文字列を指定
             $filter->like('name', 'カテゴリー名');
-            $filter->like('major_category_name', '親カテゴリー名');
+            $filter->like('major_category_name', '親カテゴリー名')->multipleSelect(MajorCategory::all()->pluck('name', 'id'));
             // 範囲指定のフィルタを追加する関数です。datetime()を付与することで、カレンダーを表示して指定できる
             $filter->between('created_at', '登録日')->datetime();
         });
@@ -59,7 +60,7 @@ class CategoryController extends AdminController
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
         $show->field('description', __('Description'));
-        $show->field('major_category_name', __('Major category name'));
+        $show->field('major_category_id', __('Major category name'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -71,13 +72,13 @@ class CategoryController extends AdminController
      *
      * @return Form
      */
-    protected function form()
+    protected function form() //新規登録・編集機能
     {
         $form = new Form(new Category());
 
         $form->text('name', __('Name'));
         $form->textarea('description', __('Description'));
-        $form->text('major_category_name', __('Major category name'));
+        $form->text('major_category_id', __('Major category Name'));
 
         return $form;
     }
