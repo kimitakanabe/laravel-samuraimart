3d0f0370b98b693230abd3baf0edefaeb8439023
kimitakanabe <kimiko.takanabe@gmail.com>
2025-08-08 16:06:16 +0200

    Create MajorCategoryController

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -6,6 +6,7 @@ use Encore\Admin\Facades\Admin;
 use Illuminate\Routing\Router;
 use App\Admin\Controllers\CategoryController;
 use App\Admin\Controllers\ProductController;
+use App\Admin\Controllers\MajorCategoryController;
 
 Admin::routes();
 
@@ -19,5 +20,6 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('categories', CategoryController::class);
     $router->resource('products', ProductController::class);
+    $router->resource('major-categories', MajorCategoryController::class);
 
 });
